214e1be1ba722fda2687220f557bfc7ee969c3ba
Huni <hunooor01@gmail.com>
2023-04-04 21:08:20 +0300

    add swagger

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -35,10 +35,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
     'djoser',
     'rest_framework',
+    'drf_yasg',
 
-    'users'
+    'users',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -129,4 +132,5 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
